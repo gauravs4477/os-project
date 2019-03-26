a6270d409d8e98b9c6ed30b16673024458a8453d
gauravs4477 <42710724+gauravs4477@users.noreply.github.com>
2019-03-27 01:27:43 +0530

    Update scan.c
    completed

--- scan.c
+++ scan.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    int a[5000];
     int b[9]={86,1470,913,1774,948,1509,1022,1750,130};
     int p=143,s=0,c=1;
     for(int i=p;i<5000;i++)
@@ -12,11 +11,14 @@ int main()
             {
                 s=s+(b[j]-p);
                 p=b[j];
+                printf("one");
             }
             if(i==4999)
             {
                 s=s+(4999-p);
                 p=143;
+                printf("two");
+                break;
             }
         }
     }
@@ -29,12 +31,14 @@ int main()
                 s=s+(4999-b[j]);
                 p=b[j];
                 c=0;
+                printf("three");
             }
             if(i==b[j])
             {
-                 s=s+(p-b[j]);
+                s=s+(p-b[j]);
+                printf("four");
             }
         }
     }
-    printf(s);
+    printf("%d",s);
 }
